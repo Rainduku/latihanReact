53e2921523d6567c40c17ed6bc2fce5912f16528
Rainduku <nasvan,chider123@gmail.com>
2025-01-06 19:02:24 +0700

    make form

---
+++ my-form/src/Form/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormElement from "./Pembahasan/FormElement";
+
+export default class Form extends React.Component {
+    render() {
+        return (
+            <FormElement />
+        )
+    }
+}
